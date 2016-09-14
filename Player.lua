7d4d45007509cc03a366c27d5106bd920a3ab7dd
danandrus <danandrus@google.com>
2016-09-13 23:30:42 -0700

    Corrected superclass calls in Player. Updated Player to extend PhysObject

--- Player.lua
+++ Player.lua
@@ -1,44 +1,73 @@
 require "common/class"
+require "PhysObject"
 
-Player = buildClass(Entity)
-Player.BOX_WIDTH = 32
-Player.BOX_HEIGHT = 32
-Player.X_STEP = 32
-Player.Y_STEP = 32
+Player = buildClass(PhysObject)
 
 function Player:_init()
-  -- Optional constructor
-  self.x = Player.BOX_WIDTH
-  self.y = Player.BOX_HEIGHT
+  Player.superclass._init(self)
   
-  self.width = Player.BOX_WIDTH
-  self.height = Player.BOX_HEIGHT
+  self.xStep = 32
+  self.yStep = 32
+
+  self:setSize(32, 32)
+  self:setPosition(self.xStep, self.yStep)
   
-  self.xStep = Player.X_STEP
-  self.yStep = Player.Y_STEP
+  self.steps = {
+       ["up"] = {x =  0, y = -1},
+    ["right"] = {x =  1, y =  0},
+     ["down"] = {x =  0, y =  1},
+     ["left"] = {x = -1, y =  0}
+  }
 end
 
 function Player:registerWithSecretary(secretary)
-  Player.superclass:registerWithSecretary(secretary)
+  Player.superclass.registerWithSecretary(self, secretary)
   
   -- Register for event callbacks
   secretary:registerEventListener(self, self.onKeyPress, EventType.KEYBOARD_DOWN)
+  secretary:registerEventListener(self, self.onKeyRelease, EventType.KEYBOARD_UP)
   secretary:registerEventListener(self, self.draw, EventType.DRAW)
 end
 
 function Player:onKeyPress(key, scancode, isrepeat)
-  if key == "up" then
-    self.y = self.y - self.yStep
-  elseif key == "right" then
-    self.x = self.x + self.xStep
-  elseif key == "down" then
-    self.y = self.y + self.yStep
-  elseif key == "left" then
-    self.x = self.x - self.xStep
+  -- only allows a single key at a time to be pressed
+  if self.key ~= nil then
+    return
+  end
+  
+  if self.steps[key] == nil then
+    return
+  end
+  self.key = key
+
+  local xStep = self.steps[key].x * self.xStep
+  local yStep = self.steps[key].y * self.yStep
+
+  local x, y = self:getPosition()
+  local xNext = x + xStep
+  local yNext = y + yStep
+
+  local t, r, b, l = self:getBoundingBox(xStep, yStep)
+  local collisions = self:getSecretary():getCollisions(t, r, b, l, Wall)
+  
+  -- Cancel jump if we would collide with a wall
+  if table.getn(collisions) > 0 then
+    return
+  end
+  
+  self:setPosition(xNext, yNext)
+  self:getSecretary():updateObject(self)
+end
+
+function Player:onKeyRelease(key, scancode)
+  if key == self.key then
+    self.key = nil
   end
 end
 
 function Player:draw()
   love.graphics.setColor(63, 63, 255)
-  love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+  local x, y = self:getPosition()
+  local w, h = self:getSize()
+  love.graphics.rectangle("fill", x, y, w, h)
 end
