4c2e1bc128b9f8573bc95d7fe2d5319458db5acd
danandrus <danandrus@google.com>
2016-09-13 23:41:58 -0700

    Added walls to game

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 require "config"
 require "Secretary"
 require "Player"
+require "Wall"
 
 rootSecretary = Secretary()
 
@@ -8,4 +9,26 @@ require "loveevents"
 
 function love.load()
   player = Player():registerWithSecretary(rootSecretary)
+  buildWalls()
+end
+
+function buildWalls()
+  local wallCodes = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+  }
+  
+  for wallY,row in ipairs(wallCodes) do
+    for wallX,wallCode in ipairs(row) do
+      if wallCode == 1 then
+        local newWall = Wall()
+        newWall:setPosition((wallX - 1) * 32, (wallY - 1) * 32)
+        newWall:registerWithSecretary(rootSecretary)
+      end
+    end
+  end
 end
