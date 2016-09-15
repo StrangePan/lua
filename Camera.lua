0a9b6ee612bd6130b1d5efa8393c4e9f506617e5
danandrus <danandrus@google.com>
2016-09-14 20:40:48 -0700

    Added easing to camera

--- Camera.lua
+++ Camera.lua
@@ -8,6 +8,8 @@ function Camera:_init()
   
   self.x = 0
   self.y = 0
+  self.drawX = self.x
+  self.drawY = self.y
 end
 
 function Camera:moveTo(x, y)
@@ -20,11 +22,27 @@ end
 function Camera:registerWithSecretary(secretary)
   Camera.superclass.registerWithSecretary(self, secretary)
   secretary:registerEventListener(self, self.onPreDraw, EventType.PRE_DRAW)
+  secretary:registerEventListener(self, self.onPostPhysics, EventType.POST_PHYSICS)
   return self
 end
 
 function Camera:onPreDraw()
   love.graphics.origin()
   local width, height = love.graphics.getDimensions()
-  love.graphics.translate((width/2)-self.x, (height/2)-self.y)
+  love.graphics.translate((width/2)-self.drawX, (height/2)-self.drawY)
+end
+
+function Camera:onPostPhysics()
+  local snapThreshold = 0.0078125
+  local easeRatio = 0.25
+  if math.abs(self.drawX - self.x) < snapThreshold then
+    self.drawX = self.x
+  else
+    self.drawX = self.drawX - (self.drawX - self.x) * easeRatio
+  end
+  if math.abs(self.drawY - self.y) < snapThreshold then
+    self.y = self.drawX
+  else
+    self.drawY = self.drawY - (self.drawY - self.y) * easeRatio
+  end
 end
