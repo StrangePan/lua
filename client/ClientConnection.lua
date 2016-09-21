9506a781e0385cc3c5133cb1c3ade1e5a8a8d7ee
danandrus <danandrus@google.com>
2016-09-21 11:37:58 -0600

    Added ClientConnection object for handling server communications

---
+++ client/ClientConnection.lua
@@ -0,0 +1,84 @@
+require "common/class"
+require "MessageReceiver"
+require "MessageSender"
+require "ConnectionStatus"
+
+-- message handler and coordinator for clients
+
+ClientConnection = buildClass()
+
+--
+-- Initializes a new connection object optimized for clients. Registers
+-- as a listener for messageReceiver's messages and automaticaly handles
+-- communication with server.
+--
+function ClientConnection:_init()
+  
+  self.serverAddress = '127.0.0.1'
+  self.serverPort = 25565
+  
+  -- Initialize udp connection object
+  self.socket = require "socket"
+  self.udp = socket:udp()
+  self.udp:settimeout(0)
+  local res,err = self.udp:setsockname('*', 25565)
+  if res == nil then
+    print(err)
+    return
+  end
+  
+  -- Initialize status variables
+  self.connectionStatus = ConnectionStatus.DISCONNECTED
+  self.connectionid = nil
+  
+  -- Initialize sender/receiver objects
+  self.sender = MessageSender(self.udp)
+  self.receiver = MessageReceiver(self.udp)
+  
+  -- Register for message callbacks
+  self.receiver:registerListener(MessageType.SERVER_CONNECT_ACK,
+    self, self.onReceiveServerConnectAck)
+end
+
+--
+-- Gets the current status of the connection with the server.
+-- Returns a ConnectionStatus enum value.
+--
+function ClientConnection:getConnectionStatus()
+  return self.connectionStatus
+end
+
+--
+-- Changes the connection status and notifies registered listeners if
+-- necessary.
+--
+function ClientConnection:setConnectionStatus(status)
+  self.connectionStatus = status
+end
+
+--
+-- Reads and processes incoming messages
+--
+function ClientConnection:processIncomingMessages()
+  self.receiver:processIncomingMessages()
+end
+
+--
+-- Handles an incoming message of type MessageType.SERVER_CONNECT_ACK
+--
+function ClientConnection:onReceiveServerConnectAck(message, address, port)
+  if self:shouldIgnore(message, address, port) then return end
+  
+  -- ignore message if not looking to connect
+  if self:getConnectionStatus() ~= ConnectionStatus.CONNECTING then return end
+  
+  self.connectionid = message.id
+  self:setConnectionStatus(ConnectionStatus.CONNECTED)
+end
+
+--
+-- returns true if the given message should not be processed
+--
+function ClientConnection:shouldIgnore(message, address, port)
+  return address ~= self.serverAddress or port ~= self.serverPort
+end
--- client/main.lua
+++ client/main.lua
@@ -4,10 +4,13 @@ require "Secretary"
 require "Player"
 require "Wall"
 require "Camera"
+require "ClientConnection"
 
 rootSecretary = Secretary()
 
 require "loveevents"
+
+local connection = ClientConnection()
 
 function love.load()
   camera = Camera():registerWithSecretary(rootSecretary)
@@ -18,7 +21,11 @@ function love.load()
       local px, py = player:getPosition()
       local pw, ph = player:getSize()
       camera:moveTo(px+(pw/2), py+(ph/2))
-    end, EventType.PRE_PHYSICS)
+    end, EventType.STEP)
+  
+  rootSecretary:registerEventListener({}, function()
+      connection:processIncomingMessages()
+    end, EventType.STEP)
 end
 
 function buildWalls()
@@ -28,7 +35,13 @@ function buildWalls()
     {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1},
     {1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    {1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+    {1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
   }
   
   for wallY,row in ipairs(wallCodes) do
