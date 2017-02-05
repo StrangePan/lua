b22efe27993dabc60b3fa2f0f48fa1fc08e7c237
Dan Andrus <danandrus@gmail.com>
2017-02-05 13:43:35 -0800

    Added collectible entity. I like the animation.

---
+++ common/entities/Collectable.lua
@@ -0,0 +1,53 @@
+require "PhysObject"
+require "Color"
+require "MazerinoTranslations"
+
+Switch = buildClass(PhysObject)
+
+function Switch:_init(x, y)
+  Switch.superclass._init(self)
+  self:setPosition(x, y)
+  self:setSize(1, 1)
+  self.animationTime = 0
+  self.animationStep = 1/512
+  self.color = Color(255, 255, 255)
+end
+
+function Switch:registerWithSecretary(secretary)
+  Switch.superclass.registerWithSecretary(self, secretary)
+  secretary:registerEventListener(self, self.step, EventType.STEP)
+  secretary:registerEventListener(self, self.draw, EventType.DRAW)
+end
+
+function Switch:step()
+  self.animationTime = self.animationTime + self.animationStep
+  while self.animationTime >= 1 do
+    self.animationTime = self.animationTime - 1
+  end
+end
+
+function Switch:draw()
+  love.graphics.push()
+  local x, y = toScreen(self:getPosition())
+  local w, h = toScreen(self:getSize())
+  local ow = w/2
+  local oh = h/2
+  love.graphics.setColor(self.color:getRGBA())
+  love.graphics.translate(x + ow, y + oh)
+  
+  -- inner square
+  love.graphics.push()
+  local innerScale = 0.5
+  love.graphics.rotate(self.animationTime * (math.pi*2))
+  love.graphics.rectangle("fill", -ow * innerScale, -oh * innerScale, w * innerScale, h * innerScale)
+  love.graphics.pop()
+  
+  -- outer box
+  love.graphics.push()
+  local outerScale = 0.625
+  love.graphics.rotate(-self.animationTime * 1.25 * (math.pi*2))
+  love.graphics.rectangle("line", -ow * outerScale, -oh * outerScale, w * outerScale, h * outerScale)
+  love.graphics.pop()
+  
+  love.graphics.pop()
+end
