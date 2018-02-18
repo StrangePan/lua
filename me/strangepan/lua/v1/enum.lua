80cf078075cd59643e983f2c96325cdce35c2123
Dan Andrus <danandrus@gmail.com>
2018-02-17 17:31:48 -0800

    Add enum to me.strangepan.lua

---
+++ me/strangepan/lua/v1/enum.lua
@@ -0,0 +1,15 @@
+local assert_that = require 'me.strangepan.lua.truth.v1.assert_that'
+local enum = {}
+
+function enum.build(...)
+  local new_enum = {...}
+  local n = 1
+  while new_enum[n] ~= nil do
+    new_enum[assert_that(new_enum[n]):is_a_string():and_return()] = n
+    n = n + 1
+  end
+  new_enum.n = n - 1
+  return new_enum
+end
+
+return enum
---
+++ me/strangepan/lua/v1/test/enum_test.lua
@@ -0,0 +1,30 @@
+local luaunit = require 'luaunit'
+local enum = require 'me.strangepan.lua.v1.enum'
+
+TestObject = {}
+
+function TestObject:test_build_whenEmpty_isEmpty()
+  luaunit.assertEquals(enum.build().n, 0)
+end
+
+function TestObject:test_build_withData_didSetN()
+  luaunit.assertEquals(enum.build('A', 'B', 'C').n, 3)
+end
+
+function TestObject:test_build_withData_didAssignIndices()
+  local under_test = enum.build('A', 'B', 'C')
+
+  luaunit.assertEquals(under_test.A, 1)
+  luaunit.assertEquals(under_test.B, 2)
+  luaunit.assertEquals(under_test.C, 3)
+end
+
+function TestObject:test_build_withData_canAccessViaIndices()
+  local under_test = enum.build('A', 'B', 'C')
+
+  luaunit.assertEquals(under_test[1], 'A')
+  luaunit.assertEquals(under_test[2], 'B')
+  luaunit.assertEquals(under_test[3], 'C')
+end
+
+os.exit(luaunit.LuaUnit.run())
