64a038c87db68ec43bf2d6c9a5b0b1a3729175cd
Dan Andrus <danandrus@gmail.com>
2018-07-15 21:46:47 -0700

    Add documetation to assertions.lua

--- me/strangepan/libs/lua/truth/v1/assertions.lua
+++ me/strangepan/libs/lua/truth/v1/assertions.lua
@@ -1,5 +1,11 @@
 local assertion = require 'me.strangepan.libs.lua.truth.v1.assertion'
 
+--[[ Collection of assertions used in other parts of this library and in external code.
+
+It is preferable that external code use the `assert_that` style of assertions rather than these
+simple functions, but these are still provided for convenience and efficiency.
+]]
+
 local assertions = {}
 
 assertions.is_a_string = assertion.of_type('string')
