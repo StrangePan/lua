5d96784a578c40caab82e4184bba033e82dcf8a3
Daniel Andrus <danieleandrus@gmail.com>
2019-05-13 19:38:00 -0700

    Add physics file to generalize computation of linear derivatives of a point on a dimension

---
+++ me/strangepan/games/spacecargo/newton/physics.lua
@@ -0,0 +1,32 @@
+local Physics = {}
+
+local ftable = {n = 0, [1] = 1, [2] = 2}
+
+local function factorial(n)
+  if n <= ftable.n then
+    return ftable[n]
+  end
+  for i = ftable.n + 1, n do
+    ftable[i] = ftable[i - 1] * i
+  end
+  return ftable[n]
+end
+
+function Physics.increment(dt, ...)
+  local initial_state = {...}
+  local n = #initial_state
+  local processed_state = {}
+  local dts = {}
+  for i = n, 1, -1 do
+    local k = initial_state[i]
+    if i < n then
+      local j = n - i
+      dts[j] = dt ^ j / factorial(j)
+    end
+    for j = i + 1, n do
+      k = k + initial_state[j] * dts[j - i]
+    end
+    processed_state[i] = k
+  end
+  return unpack(processed_state)
+end
